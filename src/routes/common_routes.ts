0a6963ff43507e0389aba340a64e7faac24fbceb
Kanstantin Kachanouski <kanstantin_kachanouski@epam.com>
2021-02-02 21:43:38 +0300

    Moved lib -> src folder

--- src/routes/common_routes.ts
+++ src/routes/common_routes.ts
@@ -1,6 +1,10 @@
 import { Application, Request, Response } from 'express';
+import * as path from "path";
 export class CommonRoutes {
    public route(app: Application) {
+      app.get('/index.html', function(req, res) {
+         res.sendFile('index.html', {root: './'});
+      });
       // Mismatch URL
       app.all('*', function (req: Request, res: Response) {
          res.status(404).send({ error: true, message: 'Check your URL please' });
